3374d68cd9313eba4f9c54b584d6e81306e237d1
Hicaru <hicc0rus@gmail.com>
2022-11-08 17:55:36 +0700

    added genesis blokcchain

--- package/blockchain.py
+++ package/blockchain.py
@@ -7,3 +7,13 @@ class BlockChain:
     def __init__(self):
         self.chain = [Epoch(0, [], zero_512_hash)]
 
+    def get_current_epoch(self):
+        return self.chain[-1]
+
+    def get_epoch(self, epoch_number):
+        return self.chain[epoch_number]
+
+
+if __name__ == '__main__':
+    # testing
+    chain = BlockChain()
