a44c8f240fe0597934e2d42a8816396cfb7b66f5
Hicaru <hicc0rus@gmail.com>
2022-11-08 16:01:10 +0700

    added block class

---
+++ package/block.py
@@ -0,0 +1,24 @@
+import hashlib
+import json
+
+
+class Block:
+    def __init__(self, id: int, transactions: list, creater: str, previous_hash: str, epoch: str):
+        self.blocknumber = id
+        self.epoch = epoch
+        self.previous_hash = previous_hash
+        self.transactions = transactions
+        self.creater = creater
+        self.hash = hashlib.sha256(
+            self.blocknumber + ','.join(str(x) for x in self.transactions) + self.creater + self.previous_hash + self.epoch
+        ).hexdigest()
+
+    def serialize(self):
+        return json.dumps({
+            "blocknumber": self.blocknumber,
+            "transactions": self.transactions,
+            "creater": self.creater,
+            "hash": self.hash,
+            "previous_hash": self.previous_hash,
+            "epoch": self.epoch
+        })
--- package/config.py
+++ package/config.py
@@ -1,1 +1,3 @@
-bootstrap = []
+bootstrap = [
+  "127.0.0.1:10001"
+]
